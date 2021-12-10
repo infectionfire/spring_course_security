17be5d18eb4629f75a438f0d278ddf64febcf170
Infectionfire <infectionfire0911@gmail.com>
2021-12-10 17:15:08 +0300

    Added a method for taking passwords from the database

--- src/main/java/com/ivanovsergei/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/ivanovsergei/spring/security/configuration/MySecurityConfig.java
@@ -1,32 +1,43 @@
 package com.ivanovsergei.spring.security.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity//помечаем класс, как класс, ответственный за конфигурацию секьюрити, конфигурейшен аннотацию не пишем
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public DataSource dataSource;
+
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //теперь спринг знает, как брать инфу из бд
+        auth.jdbcAuthentication().dataSource(dataSource);
 
-
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();//временный метод, декодер в памяти
-        //плохая практика, подходит для тестов
-        auth.inMemoryAuthentication().withUser(userBuilder
-                .username("sergei")
-                .password("sergei")
-                .roles("EMPLOYEE"));
-        auth.inMemoryAuthentication().withUser(userBuilder
-                .username("oleg")
-                .password("oleg")
-                .roles("HR"));
-        auth.inMemoryAuthentication().withUser(userBuilder
-                .username("semen")
-                .password("semen")
-                .roles("MANAGER"));
+//
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();//временный метод, декодер в памяти
+//        //плохая практика, подходит для тестов
+//        auth.inMemoryAuthentication().withUser(userBuilder
+//                .username("sergei")
+//                .password("sergei")
+//                .roles("EMPLOYEE"));
+//        auth.inMemoryAuthentication().withUser(userBuilder
+//                .username("oleg")
+//                .password("oleg")
+//                .roles("HR"));
+//        auth.inMemoryAuthentication().withUser(userBuilder
+//                .username("semen")
+//                .password("semen")
+//                .roles("MANAGER"));
 
     }
 
